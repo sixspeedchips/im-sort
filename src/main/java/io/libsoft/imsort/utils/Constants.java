0ed99d165d678684adbd194cc05fcceeb6b732c2
swandivejack <isaaclindland@protonmail.com>
2020-02-04 22:26:02 -0700

    8 sorts now working

--- src/main/java/io/libsoft/imsort/utils/Constants.java
+++ src/main/java/io/libsoft/imsort/utils/Constants.java
@@ -2,7 +2,7 @@ package io.libsoft.imsort.utils;
 
 public class Constants {
 
-  public static final int SIZE = 900;
+  public static final int SIZE = 400;
   public static final int ITERATIONS = (int) Math.sqrt(SIZE) + 10;
   public static final int MAX_THREADS = 32;
 }
